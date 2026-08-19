c5b069b2d9afd2026ec7a16befb6cc36e434b867
luisalpastor <mrluisfeer@gmail.com>
2026-08-19 13:14:07 -0600

    refactor: update component imports and improve styling consistency
    - Refactored dropdown-menu, input, kbd, label, scroll-area, separator, switch, tooltip components to use consistent import syntax and updated class names for better styling.
    - Enhanced accessibility and usability by adjusting class names and props across various components.
    - Updated global CSS variables for improved theming and visual consistency.
    - Removed unnecessary comments and streamlined component structures for better readability.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
 import { ThemeProvider } from '@/components/theme-provider';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import '@/styles/globals.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Inter } from 'next/font/google';
+import { JetBrains_Mono } from 'next/font/google';
 
-const inter = Inter({
+const jetBrainsMono = JetBrains_Mono({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-inter',
+  variable: '--font-jetbrains-mono',
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning className={inter.variable}>
-      <body className={inter.className}>
+    <html lang="en" suppressHydrationWarning className={jetBrainsMono.variable}>
+      <body className={jetBrainsMono.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          <TooltipProvider>
           {children}
+          </TooltipProvider>
         </ThemeProvider>
         <SpeedInsights />
       </body>
